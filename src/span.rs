daf062b7679a9adc6d0474a9bb4cfc3e1754cb56
Fahmy Mohammed <fahmyh.mohammed@gmail.com>
2020-08-20 14:02:30 -0700

    Add Span attributes (#17)

--- src/span.rs
+++ src/span.rs
@@ -1,3 +1,6 @@
+use crate::attribute::{ToValue, Value};
+use std::collections::HashMap;
+
 /// Represents a distributed tracing span.
 #[derive(serde::Serialize, Debug, PartialEq)]
 pub struct Span {
@@ -7,6 +10,9 @@ pub struct Span {
     trace_id: String,
 
     timestamp: u64,
+
+    #[serde(skip_serializing_if = "HashMap::is_empty")]
+    attributes: HashMap<String, Value>,
 }
 
 impl Span {
@@ -16,6 +22,7 @@ impl Span {
             id: id.to_string(),
             trace_id: trace_id.to_string(),
             timestamp: timestamp,
+            attributes: HashMap::new(),
         }
     }
 
@@ -49,11 +56,60 @@ impl Span {
     pub fn set_timestamp(&mut self, timestamp: u64) {
         self.timestamp = timestamp;
     }
+
+    /// Set the name of this span.
+    pub fn name(self, name: &str) -> Self {
+        self.attribute("name", name)
+    }
+
+    pub fn set_name(&mut self, name: &str) {
+        self.set_attribute("name", name);
+    }
+
+    /// Set the duration (in milliseconds) of this span.
+    pub fn duration(self, duration: u64) -> Self {
+        self.attribute("duration.ms", duration)
+    }
+
+    pub fn set_duration(&mut self, duration: u64) {
+        self.set_attribute("duration.ms", duration);
+    }
+
+    /// Set the id of the previous caller of this span.
+    pub fn parent_id(self, parent_id: &str) -> Self {
+        self.attribute("parent.id", parent_id)
+    }
+
+    pub fn set_parent_id(&mut self, parent_id: &str) {
+        self.set_attribute("parent.id", parent_id);
+    }
+
+    /// Set the name of the service that created this span.
+    pub fn service_name(self, service_name: &str) -> Self {
+        self.attribute("service.name", service_name)
+    }
+
+    pub fn set_service_name(&mut self, service_name: &str) {
+        self.set_attribute("service.name", service_name);
+    }
+
+    /// Set an attribute on the span.
+    pub fn attribute<T: ToValue>(mut self, key: &str, value: T) -> Self {
+        self.attributes
+            .insert(key.to_string(), value.to_attribute_value());
+        self
+    }
+
+    pub fn set_attribute<T: ToValue>(&mut self, key: &str, value: T) {
+        self.attributes
+            .insert(key.to_string(), value.to_attribute_value());
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::Span;
+    use crate::attribute::Value;
     use serde_json::json;
 
     #[test]
@@ -94,10 +150,134 @@ mod tests {
 
     #[test]
     fn span_to_json() {
-        // Check span JSON serialization.
-        let span = Span::new("id1", "traceId1", 1);
+        // Check span JSON serialization with empty attribute hashmap.
+        let mut span = Span::new("id1", "traceId1", 1);
         let json_span = json!({"id": "id1", "trace.id": "traceId1", "timestamp": 1});
 
         assert_eq!(json!(span), json_span);
+
+        // Check span JSON serialization with populated attribute hashmap.
+        span.set_name("I have a name");
+        let json_span_attribute = json!({"id": "id1", "trace.id": "traceId1", "timestamp": 1, "attributes": {"name": "I have a name"}});
+
+        assert_eq!(json!(span), json_span_attribute);
+    }
+
+    #[test]
+    fn test_attribute() {
+        let mut span = Span::new("id", "traceId", 1);
+
+        // Test name attribute
+        span.set_name("name");
+        assert_eq!(
+            span.attributes.get("name"),
+            Some(&Value::Str(String::from("name")))
+        );
+
+        span = span.name("name2");
+        assert_eq!(
+            span.attributes.get("name"),
+            Some(&Value::Str(String::from("name2")))
+        );
+
+        // Test duration attribute
+        span.set_duration(1);
+        assert_eq!(span.attributes.get("duration.ms"), Some(&Value::UInt(1)));
+
+        span = span.duration(2);
+        assert_eq!(span.attributes.get("duration.ms"), Some(&Value::UInt(2)));
+
+        // Test parent id attribute
+        span.set_parent_id("parent");
+        assert_eq!(
+            span.attributes.get("parent.id"),
+            Some(&Value::Str(String::from("parent")))
+        );
+
+        span = span.parent_id("parent2");
+        assert_eq!(
+            span.attributes.get("parent.id"),
+            Some(&Value::Str(String::from("parent2")))
+        );
+
+        // Test service name attribute
+        span.set_service_name("serviceName");
+        assert_eq!(
+            span.attributes.get("service.name"),
+            Some(&Value::Str(String::from("serviceName")))
+        );
+
+        span = span.service_name("serviceName2");
+        assert_eq!(
+            span.attributes.get("service.name"),
+            Some(&Value::Str(String::from("serviceName2")))
+        );
+    }
+
+    #[test]
+    fn test_attribute_type() {
+        let mut span = Span::new("id", "traceId", 1);
+
+        // Test String attribute
+        span.set_attribute("attr.str", "str");
+        assert_eq!(
+            span.attributes.get("attr.str"),
+            Some(&Value::Str(String::from("str")))
+        );
+
+        span = span.attribute("attr.str", "str2");
+        assert_eq!(
+            span.attributes.get("attr.str"),
+            Some(&Value::Str(String::from("str2")))
+        );
+
+        // Test UInt attribute
+        let val_u32: u32 = 5;
+        span.set_attribute("attr.uint", val_u32);
+        assert_eq!(
+            span.attributes.get("attr.uint"),
+            Some(&Value::UInt(val_u32 as u64))
+        );
+
+        let val_u64: u64 = 42;
+        span = span.attribute("attr.uint", val_u64);
+        assert_eq!(
+            span.attributes.get("attr.uint"),
+            Some(&Value::UInt(val_u64))
+        );
+
+        // Test Int attribute
+        let val_i32: i32 = -5;
+        span.set_attribute("attr.int", val_i32);
+        assert_eq!(
+            span.attributes.get("attr.int"),
+            Some(&Value::Int(val_i32 as i64))
+        );
+
+        let val_i64: i64 = -42;
+        span = span.attribute("attr.int", val_i64);
+        assert_eq!(span.attributes.get("attr.int"), Some(&Value::Int(val_i64)));
+
+        // Test Float attribute
+        let val_f32: f32 = 3.14;
+        span.set_attribute("attr.float", val_f32);
+        assert_eq!(
+            span.attributes.get("attr.float"),
+            Some(&Value::Float(val_f32 as f64))
+        );
+
+        let val_f64: f64 = 6.28;
+        span = span.attribute("attr.float", val_f64);
+        assert_eq!(
+            span.attributes.get("attr.float"),
+            Some(&Value::Float(val_f64))
+        );
+
+        // Test Bool attribute
+        span.set_attribute("attr.bool", true);
+        assert_eq!(span.attributes.get("attr.bool"), Some(&Value::Bool(true)));
+
+        span = span.attribute("attr.bool", false);
+        assert_eq!(span.attributes.get("attr.bool"), Some(&Value::Bool(false)));
     }
 }
