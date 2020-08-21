b6083e04664f1f4c866947fa5938acb5a63afaf0
Johannes Tax <jtax@newrelic.com>
2020-08-21 14:14:20 -0700

    Add blocking client and TLS switch

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
 pub mod attribute;
-#[cfg(feature = "client")]
-mod client;
+
 pub mod span;
+pub use span::{Span, SpanBatch};
 
 #[cfg(feature = "client")]
+mod client;
+#[cfg(feature = "client")]
 pub use client::{Client, ClientBuilder};
+
+#[cfg(feature = "blocking")]
+pub mod blocking {
+    pub use super::client::blocking::Client;
+}
