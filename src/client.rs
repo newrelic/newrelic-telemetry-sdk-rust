31b3183c111f9f545f71e33fcb844098f1ccf3ba
Johannes Tax <jtax@newrelic.com>
2020-08-21 08:47:31 -0700

    Add send method for traces

--- src/client.rs
+++ src/client.rs
@@ -6,6 +6,7 @@ use hyper::client::HttpConnector;
 use hyper::header::{CONTENT_ENCODING, CONTENT_TYPE, USER_AGENT};
 use hyper::{Body, HeaderMap, Method, Request, Response, Uri};
 use hyper_tls::HttpsConnector;
+use crate::span::SpanBatch;
 use log::{debug, error, info};
 use std::future::Future;
 use std::io::Write;
